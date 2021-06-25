8ba593e958a2fb46342deb8b8abf5c2cf5eb7aa0
Igor Duarte <igor.duartematos@gmail.com>
2021-06-25 11:42:59 -0300

    feat: test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -68,7 +68,7 @@ export function Home() {
           src={illustrationImg}
           alt="Ilustração simbolizando perguntas e respostas"
         />
-        <strong>Crie salas de Q&amp;A ao vivo</strong>
+        <strong>Teste Crie salas de Q&amp;A ao vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo real</p>
       </aside>
       <main>
